bb01c9eeb9188eb9296f5e8170d765d55fa507bf
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-11-05 20:05:15 +0700

    useSelector to get the state

--- src/features/iceCream/IcreCreamView.jsx
+++ src/features/iceCream/IcreCreamView.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const IcreCreamView = () => {
+  const numOfIceCreams = useSelector((state) => state.icecream.numOfIceCreams);
+
   return (
     <div>
-      <h2>Number of ice creams -</h2>
+      <h2>Number of ice creams - {numOfIceCreams}</h2>
       <button>Order ice cream</button>
       <button>Restock ice creams</button>
     </div>
